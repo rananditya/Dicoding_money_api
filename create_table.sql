e17915910d882b1325411860bdae2c7aa146f464
Dava Rananditya <109863358+rananditya@users.noreply.github.com>
2023-05-15 19:16:56 +0700

    Add files via upload

---
+++ create_table.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE "database-dicoding-web-app";
+USE database dicoding-web-app;
+CREATE TABLE records (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(25) NOT NULL,
+    amount DOUBLE NOT NULL,
+    date DATETIME NOT NULL,
+    notes TEXT,
+    attachment VARCHAR(255)
+);
